068af5261c1f678bd5a9ab6c8fe7eb36b0e17a22
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-07-03 21:22:10 +0900

    feat : css 적용, 전체적인 틀 완성

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <section class="todoapp">
+    <div>
+      <h1>TODOS</h1>
+      <input
+        id="new-todo-title"
+        class="new-todo"
+        placeholder="할일을 추가해주세요"
+        autofocus
+      />
+    </div>
+    <div class="main">
+      <input class="toggle-all" type="checkbox" />
+      <ul id="todo-list" class="todo-list"></ul>
+    </div>
+    <div class="count-container">
+      <span class="todo-count">총 <strong>0</strong> 개</span>
+      <ul class="filters">
+        <li>
+          <a class="all selected">전체보기</a>
+        </li>
+        <li>
+          <a class="active">해야할 일</a>
+        </li>
+        <li>
+          <a class="completed">완료한 일</a>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
