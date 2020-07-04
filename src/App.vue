3b39e474df400b862d9efbc01cf634479184733a
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-07-04 18:02:01 +0900

    feat : 수정, 수정취소, 남은할일개수 표시, Todo유형에 따라 선별 출력 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -15,30 +15,54 @@
       <input class="toggle-all" type="checkbox" />
       <ul id="todo-list" class="todo-list">
         <li
-          v-for="(item, index) in todoItems"
-          :class="item.completed ? 'completed' : 'active'"
+          v-for="(item, index) in filteredTodoItems"
+          :class="{ completed: item.isCompleted, editing: item.isEditing }"
           :key="index"
+          @dblclick="toggleEditingById(item.id)"
         >
           <div class="view">
-            <input class="toggle" type="checkbox" v-model="item.completed" />
+            <input class="toggle" type="checkbox" v-model="item.isCompleted" />
             <label class="label">{{ item.title }}</label>
             <button class="destroy" @click="deleteById(item.id)"></button>
           </div>
-          <input class="edit" :value="item.title" />
+          <input
+            class="edit"
+            v-model="item.titleToEdit"
+            @keyup.enter="editById(item.id, item.titleToEdit)"
+            @keyup.esc="cancelEditingById(item.id)"
+          />
         </li>
       </ul>
     </div>
     <div class="count-container">
-      <span class="todo-count">총 <strong>0</strong> 개</span>
+      <span class="todo-count"
+        >남은 할 일 <strong>{{ activeTodoItemsSize }}</strong
+        >개</span
+      >
       <ul class="filters">
         <li>
-          <a class="all selected">전체보기</a>
+          <a
+            class="all"
+            @click="changeStatus('all')"
+            :class="{ selected: status === 'all' }"
+            >전체보기</a
+          >
         </li>
         <li>
-          <a class="active">해야할 일</a>
+          <a
+            class="active"
+            @click="changeStatus('active')"
+            :class="{ selected: status === 'active' }"
+            >해야할 일</a
+          >
         </li>
         <li>
-          <a class="completed">완료한 일</a>
+          <a
+            class="completed"
+            @click="changeStatus('completed')"
+            :class="{ selected: status === 'completed' }"
+            >완료한 일</a
+          >
         </li>
       </ul>
     </div>
@@ -53,6 +77,7 @@ export default {
       autoIncrementId: 0,
       inputText: "",
       todoItems: [],
+      status: "all",
     };
   },
   methods: {
@@ -63,7 +88,9 @@ export default {
       const newTodo = {
         id: ++this.autoIncrementId,
         title: this.inputText.trim(),
-        completed: false,
+        isCompleted: false,
+        isEditing: false,
+        titleToEdit: this.inputText.trim(),
       };
       this.todoItems.push(newTodo);
       this.inputText = "";
@@ -72,7 +99,42 @@ export default {
       const index = this.todoItems.findIndex((item) => item.id === id);
       this.todoItems.splice(index, 1);
     },
+    toggleEditingById(id) {
+      const index = this.todoItems.findIndex((item) => item.id === id);
+      let changedTodo = this.todoItems[index];
+      changedTodo.isEditing = !changedTodo.isEditing;
+      this.todoItems.splice(index, 1, changedTodo);
+    },
+    editById(id, title) {
+      const index = this.todoItems.findIndex((item) => item.id === id);
+      let changedTodo = this.todoItems[index];
+      changedTodo.title = title;
+      this.todoItems.splice(index, 1, changedTodo);
+      this.toggleEditingById(id);
+    },
+    cancelEditingById(id) {
+      const index = this.todoItems.findIndex((item) => item.id === id);
+      this.todoItems[index].titleToEdit = this.todoItems[index].title;
+      this.toggleEditingById(id);
+    },
+    changeStatus(status) {
+      this.status = status;
+    },
   },
-  components: {},
+  computed: {
+    filteredTodoItems() {
+      switch (this.status) {
+        case "active":
+          return this.todoItems.filter((item) => !item.isCompleted);
+        case "completed":
+          return this.todoItems.filter((item) => item.isCompleted);
+        default:
+          return this.todoItems;
+      }
+    },
+    activeTodoItemsSize() {
+      return this.todoItems.filter((item) => !item.isCompleted).length;
+    },
+  },
 };
 </script>
