773a0dc25a107b04e243c199ef0b2bab75286859
SeYun <ksy90101@naver.com>
2020-07-05 15:49:33 +0900

    feat: 수정 구현

--- vue-todo-list/src/components/todoFilters.vue
+++ vue-todo-list/src/components/todoFilters.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <ul class="filters">
-      <todo-filter v-for="filter of FILTERS" v-bind:key="filter.id" v-bind:filter="filter"></todo-filter>
+      <todo-filter
+        v-for="filter of FILTERS"
+        v-bind:key="filter.id"
+        v-bind:filter="filter"
+      ></todo-filter>
     </ul>
   </div>
 </template>
 
 <script>
 import todoFilter from "./todoFilter";
-import { FILTER } from "../utils/contants"
+import { FILTER } from "../utils/contants";
 
 export default {
   data: function() {
--- vue-todo-list/src/components/todoItem.vue
+++ vue-todo-list/src/components/todoItem.vue
@@ -1,20 +1,33 @@
 <template>
-  <li v-bind:class="classCompleted">
+  <li v-bind:class="[classCompleted, classEdit]">
     <div>
       <input class="toggle" type="checkbox" v-on:click="toggleCompleted" />
-      <label class="label">{{ todoItem.content }}</label>
+      <label class="label" v-on:dblclick="onEdit">{{ todoItem.content }}</label>
       <button class="destroy" v-on:click="todoDelete"></button>
+      <input
+        class="edit"
+        v-on:keyup.esc="cancelEditing"
+        v-on:keyup.enter="onEditing"
+        v-model="todoItem.content"
+      />
     </div>
-    <input class="edit" v-bind:value="todoItem.content" />
   </li>
 </template>
 
 <script>
 export default {
   props: ["todoItem"],
+  data: function() {
+    return {
+      isEdit: false,
+    };
+  },
   computed: {
     classCompleted: function() {
       return this.todoItem.isCompleted ? "completed" : "";
+    },
+    classEdit: function() {
+      return this.isEdit ? "editing" : "";
     },
   },
   methods: {
@@ -25,6 +38,15 @@ export default {
       if (confirm("정말로 삭제하시겠습니까?")) {
         this.$store.commit("DELETE_TODO", this.todoItem._id);
       }
+    },
+    onEdit: function() {
+      this.isEdit = true;
+    },
+    cancelEditing: function() {
+      this.isEdit = false;
+    },
+    onEditing: function() {
+      this.isEdit = false;
     },
   },
 };
